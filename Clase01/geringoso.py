90d9ef722490b774da5755d55ffc1c58b5ed56b4
agmontserrat <62036050+agmontserrat@users.noreply.github.com>
2021-04-04 22:46:50 -0300

    por las dudas un lower()

--- Clase01/geringoso.py
+++ Clase01/geringoso.py
@@ -2,8 +2,9 @@
 # Usá una iteración sobre el string cadena para agregar la sílaba 'pa', 'pe', 'pi', 'po', o 'pu'
 # según corresponda luego de cada vocal.
 
-cadena = 'Geringoso'
+cadena = 'GERINGOSO'
 capadepenapa = ''
+cadena = cadena.lower()
 for c in cadena:
     capadepenapa += c
     if c in 'aeiou':
